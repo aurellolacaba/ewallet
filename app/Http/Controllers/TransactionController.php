f4aa775009b4dc9b68785f436b3c7662560974e8
Aurello <aurellolacaba@gmail.com>
2023-04-10 14:26:59 +0800

    feat: add pagination on transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,12 +7,15 @@ use Inertia\Inertia;
 
 class TransactionController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        $limit = $request->limit ?? 10;
+
         $transactions = auth()->user()->wallet
             ->transactions()
             ->with('from', 'to')
             ->latest()
-            ->get();
+            ->paginate($limit)
+            ->withQueryString();
 
         return Inertia::render('Transaction', compact('transactions'));
     }
